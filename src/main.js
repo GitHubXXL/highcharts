216a11255483d616001c2c9a82847ae8ca25a461
xiaoxingliang <email_xxl@126.com>
2020-12-05 16:21:12 +0800

    实现highcharts的初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// import Highchart from "highcharts/highcharts";
+// import HighchartsVue from "highcharts-vue";
+// import stockInit from "highcharts/modules/stock";
+
+// stockInit(Highchart);
+// Vue.use(HighchartsVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
